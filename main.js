09b17b6451f8559c2ab4b63ab685389c64b56b46
Hifi58 <gillet.tony@gmail.com>
2021-06-11 10:54:08 +0200

    eye

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const loading = document.getElementById("loadingAnimation") ;
-const content = document.getElementById("displayContent") ;
-window.onload =setTimeout(showContent, 3000);
-function showContent() {
-    loading.style.display="none";
-    content.style.display="block";
-}
+// const loading = document.getElementById("loadingAnimation") ;
+// const content = document.getElementById("displayContent") ;
+// window.onload =setTimeout(showContent, 5000);
+// function showContent() {
+//     loading.style.display="none";
+//     content.style.display="block";
+// }
