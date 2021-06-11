e98dde104d02f80dcb7e8422cc4894243d7be7d9
Hifi58 <gillet.tony@gmail.com>
2021-06-11 09:20:03 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const loading = document.getElementById("loadingAnimation") ;
+const content = document.getElementById("displayContent") ;
+window.onload =setTimeout(showContent, 3000);
+function showContent() {
+    loading.style.display="none";
+    content.style.display="block";
+}
